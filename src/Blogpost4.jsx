3525434f132c678f80c868066ee0d9da5491cf66
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-29 10:17:53 -0400

    made things look better

--- src/Blogpost4.jsx
+++ src/Blogpost4.jsx
@@ -6,11 +6,11 @@ function Blogpost4 () {
     <div>
         <Card>
             <h1>Do you think JavaScript Frameworks and Libraries are easy or hard to use? Why?</h1>
-            <p></p>
+            <p>I Think it depends on the framework or library your using, I like React though because of its ease of use.</p>
             <h1>What Frameworks or Libraries are you interested in learning more about?</h1>
-            <p></p>
+            <p>I'd be open to learning any framwork or librayr mainly cause I didn't have a ton of time to look deep into multiple framworks and Libraries</p>
             <h1>Find one emerging JavaScript tool that you would like to learn more about or use in your final project.</h1>
-            <p></p>
+            <p>One emerging tool I'd like to use is React</p>
         </Card>
     </div>
   )
