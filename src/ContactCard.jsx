c98263c4fc7a559e68482ba952afdc223a20a624
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-30 16:34:35 -0400

    .

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -11,6 +11,8 @@ function ContactCard () {
         </div>
         <ul>
             <li><b>Jay Miller</b></li>
+            <br></br>
+            <br></br>
             <li><b>Full-Stack Developer</b></li>
         </ul>
         <div class="icon-container">
