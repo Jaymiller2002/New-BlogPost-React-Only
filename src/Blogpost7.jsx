c05a0f6387837dca426c3914b8d1027e6b12e893
Jaymiller2002 <jay.miller02@icloud.com>
2024-05-26 10:39:18 -0400

    fixed blogpost 7 and 8

--- src/Blogpost7.jsx
+++ src/Blogpost7.jsx
@@ -5,11 +5,11 @@ function Blogpost7() {
     <div>
         <Card style={{left: "90px"}}>
             <h2>What are some similarities and differences you can see with JS and Python?</h2>
-            <p></p>
+            <p>Some similarities are that they both do the same thing just with different syntax.</p>
             <h2>If you had started with Python instead of JS, how do you think the Bootcamp would be different? Would you have benefited?</h2>
-            <p></p>
+            <p>If I had started with python I don't think it would've been bad but I personally think javascript was the best approach because it's less complex than python. Python seems easy as first glance but is really complex and has a lot of similarities to Javascript.</p>
             <h2>Think of a project or tech based tool that you use often and think about what the database structure might look like for it. Do you feel like you have a better understanding of how it works now and could you see yourself enjoying building something like that in the future?</h2>
-            <p></p>
+            <p>I think of facebook or Instagram. I can see how they're database controls likes, dislikes, how many posts there are and a ton more. Yes I have a much better understanding of databases and how they work.</p>
         </Card>
     </div>
   )
--- src/Blogpost8.jsx
+++ src/Blogpost8.jsx
@@ -5,11 +5,11 @@ function Blogpost8() {
     <div>
         <Card style={{left: "90px"}}>
             <h2>What are you struggling with the most in the Tech Stack so far and why?</h2>
-            <p></p>
+            <p>I'd say i'm struugling with django and djangorestframework mainly cause it's a lot of information all at once. I'm just having trouble understanding and developing these new stacks and languages in my brain at a fast enough pace.</p>
             <h2>What companies or people (local or not) would you like to hear talk during a Bootcamp lunch and learn?</h2>
-            <p></p>
+            <p>Any company or companies that specialize in front end work. I'm not a huge fan of the backend</p>
             <h2>After having built your first full-stack application, what things in your repertoire are a sticking point for you? Were you able to reconnect your React Restaurant to your Backend Bistro code? If so, what was that like, and if not, what hurdles did you need to jump?</h2>
-            <p></p>
+            <p>Thw things that were stacking points for me was the front end because everything was much easier and you can see your changes in real time. Yes, I was able to connect my backend to my front end but I couldn't figure out how to send information back to my backend from the frontend.</p>
         </Card>
     </div>
   )
